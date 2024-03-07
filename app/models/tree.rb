9cee4d95612bc3a252e0513765a1bcd9b5ee1eaf
Oleh <zaaqq135@gmail.com>
2024-03-07 23:15:05 +0200

    added validation

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,4 +1,12 @@
 class Tree < ApplicationRecord
+  validates :instagram, presence: true, format: { with: %r{\Ahttps?://\S+\z}, message: 'Invalid URL format' }
+
+  validates :x, presence: true, format: { with: %r{\Ahttps?://\S+\z}, message: 'Invalid URL format' }
+
+  validates :youtube, presence: true, format: { with: %r{\Ahttps?://\S+\z}, message: 'Invalid URL format' }
+
+  validates :name, presence: true, length: { minimum: 4, message: 'Too short name' }
+
   extend FriendlyId
   friendly_id :name, use: :slugged
 
